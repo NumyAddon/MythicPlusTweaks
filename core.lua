d1d707709e4f750aa688d012b121e487805c86ba
Mark W <numedain@gmail.com>
2022-07-24 14:00:17 +0200

    Mayor rework to a more modular setup

--- core.lua
+++ core.lua
@@ -1,173 +1,139 @@
+local name, MPT = ...;
 
-local name, M = ...
+--@debug@
+_G.MythicPlusTweaks = MPT;
+if not _G.MPT then _G.MPT = MPT; end
+--@end-debug@
 
-local frame = CreateFrame('Frame')
-frame:HookScript('OnEvent', function(self, event, ...) M[event](M, ...); end);
-frame:RegisterEvent('ADDON_LOADED')
 
-function M:FixDungeonScoreTooltip()
-    local DUNGEON_SCORE_LINK_INDEX_START = 11;
-    local DUNGEON_SCORE_LINK_ITERATE = 3;
+local Main = LibStub('AceAddon-3.0'):NewAddon(name, 'AceConsole-3.0', 'AceHook-3.0', 'AceEvent-3.0');
+if not Main then return; end
+MPT.Main = Main;
 
-    -- copy/pasted from Blizzard's source https://github.com/Gethe/wow-ui-source/blob/978a1677c7825f6ddbf6a3f276ed61e61c6c970a/Interface/FrameXML/ItemRef.lua#L593-L664
-    -- patched with the suggestion from https://github.com/Stanzilla/WoWUIBugs/issues/242
-
-    function DisplayDungeonScoreLink(link)
-
-        if ( not ItemRefTooltip:IsShown() ) then
-            ItemRefTooltip:SetOwner(UIParent, 'ANCHOR_PRESERVE');
+function Main:OnInitialize()
+    MythicPlusTweaksDB = MythicPlusTweaksDB or {};
+    self.db = MythicPlusTweaksDB;
+    self.version = GetAddOnMetadata(name, "Version") or "";
+    self:InitDefaults();
+    for moduleName, module in self:IterateModules() do
+        if self.db.modules[moduleName] == false then
+            module:Disable();
         end
-
-        local splits  = StringSplitIntoTable(':', link);
-
-        --Bad Link, Return.
-        if(not splits) then
-            return;
-        end
-        local dungeonScore = tonumber(splits[2]);
-        local playerName = splits[4];
-        local playerClass = splits[5];
-        local playerItemLevel = tonumber(splits[6]);
-        local playerLevel = tonumber(splits[7]);
-        local className, classFileName = GetClassInfo(playerClass);
-        local classColor = C_ClassColor.GetClassColor(classFileName);
-        local runsThisSeason = tonumber(splits[8]);
-        local bestSeasonScore = tonumber(splits[9]);
-        local bestSeasonNumber = tonumber(splits[10]);
-
-        --Bad Link..
-        if(not playerName or not playerClass or not playerItemLevel or not playerLevel) then
-            return;
-        end
-
-        --Bad Link..
-        if(not className or not classFileName or not classColor) then
-            return;
-        end
-
-        GameTooltip_SetTitle(ItemRefTooltip, classColor:WrapTextInColorCode(playerName));
-        GameTooltip_AddColoredLine(ItemRefTooltip, DUNGEON_SCORE_LINK_LEVEL_CLASS_FORMAT_STRING:format(playerLevel, className), HIGHLIGHT_FONT_COLOR);
-        GameTooltip_AddNormalLine(ItemRefTooltip, DUNGEON_SCORE_LINK_ITEM_LEVEL:format(playerItemLevel));
-
-        local color = C_ChallengeMode.GetDungeonScoreRarityColor(dungeonScore) or HIGHLIGHT_FONT_COLOR;
-        GameTooltip_AddNormalLine(ItemRefTooltip, DUNGEON_SCORE_LINK_RATING:format(color:WrapTextInColorCode(dungeonScore)));
-        GameTooltip_AddNormalLine(ItemRefTooltip, DUNGEON_SCORE_LINK_RUNS_SEASON:format(runsThisSeason));
-
-        if(bestSeasonScore ~= 0) then
-            local bestSeasonColor = C_ChallengeMode.GetDungeonScoreRarityColor(bestSeasonScore) or HIGHLIGHT_FONT_COLOR;
-            GameTooltip_AddNormalLine(ItemRefTooltip, DUNGEON_SCORE_LINK_PREVIOUS_HIGH:format(bestSeasonColor:WrapTextInColorCode(bestSeasonScore), bestSeasonNumber));
-        end
-        GameTooltip_AddBlankLineToTooltip(ItemRefTooltip);
-
-        local sortTable = { };
-        for i = DUNGEON_SCORE_LINK_INDEX_START, (#splits), DUNGEON_SCORE_LINK_ITERATE do
-            local mapChallengeModeID = tonumber(splits[i]);
-            local completedInTime = tonumber(splits[i + 1]) == 1; -- patched
-            local level = tonumber(splits[i + 2]);
-
-            local mapName = C_ChallengeMode.GetMapUIInfo(mapChallengeModeID);
-
-            --If any of the maps don't exist.. this is a bad link
-            if(not mapName) then
-                return;
-            end
-
-            table.insert(sortTable, { mapName = mapName, completedInTime = completedInTime, level = level });
-        end
-
-        -- Sort Alphabetically.
-        table.sort(sortTable, function(a, b) strcmputf8i(a.mapName, b.mapName); end);
-
-        for i = 1, #sortTable do
-            local textColor = sortTable[i].completedInTime and HIGHLIGHT_FONT_COLOR or GRAY_FONT_COLOR;
-            GameTooltip_AddColoredDoubleLine(ItemRefTooltip, DUNGEON_SCORE_LINK_TEXT1:format(sortTable[i].mapName), (sortTable[i].level > 0 and  DUNGEON_SCORE_LINK_TEXT2:format(sortTable[i].level) or DUNGEON_SCORE_LINK_NO_SCORE), NORMAL_FONT_COLOR, textColor);
-        end
-        ItemRefTooltip:SetPadding(0, 0);
-        ShowUIPanel(ItemRefTooltip);
     end
 
+    self:InitConfig();
+
+    self:RegisterChatCommand('mpt', function() self:OpenConfig(); end);
 end
 
-function M:ExtendDungeonRatingTooltips()
-    hooksecurefunc(GameTooltip, 'Show', function(tooltip) M:OnTooltipShow(tooltip) end)
-end
+function Main:InitDefaults()
+    local defaults = {
+        modules = {},
+    };
 
-function M:OnTooltipShow(tooltip)
-    if self.skipOnTooltipShow or self.disable then return end
-    local owner = tooltip.GetOwner and tooltip:GetOwner() or nil
-    if not owner then return end
-
-    local parent = owner.GetParent and owner:GetParent() or nil
-    if parent ~= ChallengesFrame or not owner.mapID then return end
-
-    local mapId = owner.mapID
-    local affixScores, _ = C_MythicPlus.GetSeasonBestAffixScoreInfoForMap(mapId);
-
-    if(not affixScores or #affixScores == 0) then return end
-    local linesLeft, linesRight = self:ExtractTooltipLines(tooltip)
-    local higherScore, higherAffix = 0, nil
-    for _, affixInfo in ipairs(affixScores) do
-        if affixInfo.score > higherScore then
-            higherScore = affixInfo.score
-            higherAffix = affixInfo.name
+    for key, value in pairs(defaults) do
+        if self.db[key] == nil then
+            self.db[key] = value;
         end
-    end
-    for _, affixInfo in ipairs(affixScores) do
-        local affixName, score = affixInfo.name, affixInfo.score
-        local color = C_ChallengeMode.GetSpecificDungeonScoreRarityColor(score)
-        local multiplier = affixName == higherAffix and '|cFFFFFFFF (x1.5)|r' or '|cFFFFFFFF (x0.5)|r'
-        for i, line in ipairs(linesLeft) do
-            if string.find(line.text, affixName) then
-                table.insert(linesLeft, i+3, {text='Affix rating: ' .. color:WrapTextInColorCode(score) .. multiplier})
-                table.insert(linesRight, i+3, {text=''})
-                break
-            end
-        end
-    end
-    if(#linesLeft > 2) then
-        table.insert(linesLeft, {text='|cFFEE6161ID|r ' .. mapId})
-        table.insert(linesRight, {text=''})
-    end
-
-    tooltip:ClearLines()
-    for i = 1, max(#linesLeft, #linesRight) do
-        local left = linesLeft[i] or ''
-        local right = linesRight[i] or ''
-
-        tooltip:AddDoubleLine(left.text, right.text, left.r, left.g, left.b, right.r, right.g, right.b)
-    end
-
-    self.skipOnTooltipShow = true
-    tooltip:Show()
-    self.skipOnTooltipShow = nil
-end
-
-function M:ExtractTooltipLines(tooltip)
-    local linesLeft, linesRight = {}, {}
-    for i = 1, 15 do
-        local lineLeft = _G[tooltip:GetName() .. 'TextLeft' .. i]
-        local lineRight = _G[tooltip:GetName() .. 'TextRight' .. i]
-
-        local left, right
-        if lineLeft then left = lineLeft:GetText() end
-        if lineRight then right = lineRight:GetText() end
-
-        if not left and not right then break end
-        local leftR, leftG, leftB, _ = lineLeft:GetTextColor()
-        local rightR, rightG, rightB, _ = lineRight:GetTextColor()
-        table.insert(linesLeft, {text=left, r=leftR, g=leftG, b=leftB})
-        table.insert(linesRight, {text=right, r=rightR, g=rightG, b=rightB})
-    end
-    return linesLeft, linesRight
-end
-
-function M:ADDON_LOADED(addonName)
-    if addonName == name then
-        self:Init()
     end
 end
 
-function M:Init()
-    self:FixDungeonScoreTooltip()
-    self:ExtendDungeonRatingTooltips()
+function Main:InitConfig()
+    local count = 1;
+    local function increment() count = count + 1; return count end;
+    self.options = {
+        type = 'group',
+        name = 'Mythic+ Tweaks',
+        desc = 'Various tweaks related to mythic+',
+        childGroups = 'tab',
+        args = {
+            version = {
+                order = increment(),
+                type = 'description',
+                name = 'Version: ' .. self.version,
+            },
+            modules = {
+                order = increment(),
+                type = 'group',
+                name = 'Modules',
+                childGroups = 'tree',
+                args = {
+                    desc = {
+                        order = increment(),
+                        type = 'description',
+                        name = 'This addon consists of a number of modules, each of which can be enabled or disabled, to fine-tune your experience.',
+                    },
+                },
+            }
+        }
+    };
+    local defaultModuleOptions = {
+        type = 'group',
+        name = function(info)
+            return info[#info - 1];
+        end,
+        args = {
+            name = {
+                order = 1,
+                type = 'header',
+                name = function(info)
+                    return info.options.args.modules.args[info[#info - 1]].name;
+                end,
+            },
+            description = {
+                order = 2,
+                type = 'description',
+                name = function(info)
+                    local module = Main:GetModule(info[#info - 1]);
+                    return module.GetDescription and module:GetDescription() or '';
+                end,
+                hidden = function(info)
+                    return '' == info.option.name(info)
+                end,
+            },
+            enable = {
+                order = 3,
+                name = 'Enable',
+                desc = 'Enable this module',
+                type = 'toggle',
+                get = function(info) return Main:IsModuleEnabled(info[#info - 1]); end,
+                set = function(info, enabled) Main:SetModuleState(info[#info - 1], enabled); end,
+            },
+        },
+    };
+    for moduleName, module in self:IterateModules() do
+        local copy = CopyTable(defaultModuleOptions);
+        local moduleOptions = module.GetOptions and module:GetOptions(copy) or copy;
+        moduleOptions.name = module.GetName and module:GetName() or moduleName;
+        moduleOptions.order = increment();
+        self.options.args.modules.args[moduleName] = moduleOptions;
+    end
+
+    self.configCategory = 'Mythic+ Tweaks';
+    LibStub('AceConfig-3.0'):RegisterOptionsTable(self.configCategory, self.options);
+    LibStub("AceConfigDialog-3.0"):AddToBlizOptions(self.configCategory);
+end
+
+function Main:OpenConfig()
+    InterfaceOptionsFrame_OpenToCategory(self.configCategory);
+    InterfaceOptionsFrame_OpenToCategory(self.configCategory);
+end
+
+function Main:RegisterModuleOptions(moduleName, options)
+    self.options.args.modules.args[moduleName] = options;
+end
+
+function Main:SetModuleState(moduleName, enabled)
+    if enabled then
+        self:EnableModule(moduleName);
+    else
+        self:DisableModule(moduleName);
+    end
+    self.db.modules[moduleName] = enabled;
+end
+
+function Main:IsModuleEnabled(moduleName)
+    local module = self:GetModule(moduleName);
+
+    return module and module:IsEnabled() or false;
 end
---
+++ modules/dungeonIconTooltip.lua
@@ -0,0 +1,117 @@
+local _, MPT = ...;
+local Main = MPT.Main;
+
+local Module = Main:NewModule('DungeonIconTooltip', 'AceHook-3.0');
+
+function Module:OnEnable()
+    self:SecureHook(GameTooltip, 'Show', function(tooltip) Module:OnTooltipShow(tooltip); end);
+end
+
+function Module:OnDisable()
+    self:UnhookAll();
+end
+
+function Module:GetName()
+    return 'Dungeon Icon Tooltip';
+end
+
+function Module:GetDescription()
+    return 'Adds individual affix rating information to the dungeon icon tooltip in the LFG frame.';
+end
+
+function Module:GetOptions(defaultOptionsTable)
+    defaultOptionsTable.args.showExample = {
+        type = 'execute',
+        name = 'Open Mythic+ UI',
+        desc = 'Open the Mythic+ UI and hover over a dungeon icon to see an example.',
+        func = function()
+            PVEFrame_ToggleFrame('ChallengesFrame');
+        end,
+    };
+
+    return defaultOptionsTable;
+end
+
+function Module:OnTooltipShow(tooltip)
+    if self.skipOnTooltipShow then return end
+    local owner = tooltip.GetOwner and tooltip:GetOwner() or nil
+    if not owner then return end
+
+    local parent = owner.GetParent and owner:GetParent() or nil
+    if parent ~= ChallengesFrame or not owner.mapID then return end
+
+    local mapId = owner.mapID
+    local affixScores, _ = C_MythicPlus.GetSeasonBestAffixScoreInfoForMap(mapId);
+
+    local linesLeft, linesRight = self:ExtractTooltipLines(tooltip)
+    if(affixScores and #affixScores > 0) then
+        self:ProcessAffixScores(linesLeft, linesRight, affixScores)
+    end
+
+    if(not self:MapIdIsAddedToTooltip(linesLeft, mapId)) then
+        table.insert(linesLeft, {text='|cFFEE6161ID|r ' .. mapId})
+        table.insert(linesRight, {text=''})
+    end
+
+    tooltip:ClearLines()
+    for i = 1, max(#linesLeft, #linesRight) do
+        local left = linesLeft[i] or ''
+        local right = linesRight[i] or ''
+
+        tooltip:AddDoubleLine(left.text, right.text, left.r, left.g, left.b, right.r, right.g, right.b)
+    end
+
+    self.skipOnTooltipShow = true
+    tooltip:Show()
+    self.skipOnTooltipShow = nil
+end
+
+function Module:ProcessAffixScores(linesLeft, linesRight, affixScores)
+    local higherScore, higherAffix = 0, nil
+    for _, affixInfo in ipairs(affixScores) do
+        if affixInfo.score > higherScore then
+            higherScore = affixInfo.score
+            higherAffix = affixInfo.name
+        end
+    end
+    for _, affixInfo in ipairs(affixScores) do
+        local affixName, score = affixInfo.name, affixInfo.score
+        local color = C_ChallengeMode.GetSpecificDungeonScoreRarityColor(score)
+        local multiplier = affixName == higherAffix and '|cFFFFFFFF (x1.5)|r' or '|cFFFFFFFF (x0.5)|r'
+        for i, line in ipairs(linesLeft) do
+            if string.find(line.text, affixName) then
+                table.insert(linesLeft, i+3, {text='Affix rating: ' .. color:WrapTextInColorCode(score) .. multiplier})
+                table.insert(linesRight, i+3, {text=''})
+                break
+            end
+        end
+    end
+end
+
+function Module:MapIdIsAddedToTooltip(linesLeft, mapId)
+    for _, line in ipairs(linesLeft) do
+        if string.find(line.text, mapId) then
+            return true
+        end
+    end
+    return false
+end
+
+function Module:ExtractTooltipLines(tooltip)
+    local linesLeft, linesRight = {}, {}
+    for i = 1, 15 do
+        local lineLeft = _G[tooltip:GetName() .. 'TextLeft' .. i]
+        local lineRight = _G[tooltip:GetName() .. 'TextRight' .. i]
+
+        local left, right
+        if lineLeft then left = lineLeft:GetText() end
+        if lineRight then right = lineRight:GetText() end
+
+        if not left and not right then break end
+        local leftR, leftG, leftB, _ = lineLeft:GetTextColor()
+        local rightR, rightG, rightB, _ = lineRight:GetTextColor()
+        table.insert(linesLeft, {text=left, r=leftR, g=leftG, b=leftB})
+        table.insert(linesRight, {text=right, r=rightR, g=rightG, b=rightB})
+    end
+    return linesLeft, linesRight
+end
