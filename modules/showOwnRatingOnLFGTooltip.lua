516edb8537a19ee2a2456f29dfd2333c76185f8f
Mark W <numedain@gmail.com>
2025-10-19 23:26:51 +0200

    Update LFG data for remix keystones

--- modules/showOwnRatingOnLFGTooltip.lua
+++ modules/showOwnRatingOnLFGTooltip.lua
@@ -9,74 +9,85 @@ local Module = Main:NewModule('ShowOwnRatingOnLFGTooltip', 'AceHook-3.0');
 
 -- there is currently no in-game way to get the ChallengeModeMapId from the ActivityID, so we have to resort to a hardcoded map
 Module.ActivityIdToChallengeMapIdMap = {
-    [180] = 169, -- Iron Docks
+    [1192] = 2, -- Temple of the Jade Serpent
+    [1695] = 163, -- Bloodmaul Slag Mines
+    [1193] = 165, -- Shadowmoon Burial Grounds
     [183] = 166, -- Grimrail Depot
     [184] = 168, -- The Everbloom
+    [180] = 169, -- Iron Docks
     [459] = 197, -- Eye of Azshara
+    [1782] = 197, -- Eye of Azshara
     [460] = 198, -- Darkheart Thicket
-    [461] = 200, -- Halls of Valor
-    [462] = 206, -- Neltharion's Lair
+    [1788] = 198, -- Darkheart Thicket
     [463] = 199, -- Black Rook Hold
+    [1790] = 199, -- Black Rook Hold
+    [461] = 200, -- Halls of Valor
+    [1783] = 200, -- Halls of Valor
+    [462] = 206, -- Neltharion's Lair
+    [1785] = 206, -- Neltharion's Lair
     [464] = 207, -- Vault of the Wardens
+    [1795] = 207, -- Vault of the Wardens
     [465] = 208, -- Maw of Souls
-    [466] = 210, -- Court of Stars
+    [1787] = 208, -- Maw of Souls
     [467] = 209, -- The Arcway
+    [1791] = 209, -- The Arcway
+    [466] = 210, -- Court of Stars
+    [1789] = 210, -- Court of Stars
+    [476] = 233, -- Cathedral of Eternal Night
     [471] = 227, -- Return to Karazhan: Lower
     [473] = 234, -- Return to Karazhan: Upper
-    [476] = 233, -- Cathedral of Eternal Night
+    [1793] = 234, -- Return to Karazhan: Upper
+    [1794] = 234, -- Return to Karazhan: Upper
     [486] = 239, -- Seat of the Triumvirate
+    [1786] = 239, -- Seat of the Triumvirate
     [502] = 244, -- Atal'Dazar
+    [518] = 245, -- Freehold
+    [526] = 246, -- Tol Dagor
+    [510] = 247, -- The MOTHERLODE!!
+    [530] = 248, -- Waycrest Manor
+    [514] = 249, -- Kings' Rest
+    [661] = 249, -- Kings' Rest
     [504] = 250, -- Temple of Sethraliss
     [507] = 251, -- The Underrot
-    [510] = 247, -- The MOTHERLODE!!
-    [514] = 249, -- Kings' Rest
-    [518] = 245, -- Freehold
     [522] = 252, -- Shrine of the Storm
-    [526] = 246, -- Tol Dagor
-    [530] = 248, -- Waycrest Manor
     [534] = 353, -- Siege of Boralus
     [659] = 353, -- Siege of Boralus
-    [661] = 249, -- Kings' Rest
     [679] = 369, -- Operation: Mechagon - Junkyard
     [683] = 370, -- Operation: Mechagon - Workshop
-    [691] = 379, -- Plaguefall
+    [703] = 375, -- Mists of Tirna Scithe
+    [713] = 376, -- The Necrotic Wake
     [695] = 377, -- De Other Side
     [699] = 378, -- Halls of Atonement
-    [703] = 375, -- Mists of Tirna Scithe
+    [691] = 379, -- Plaguefall
     [705] = 380, -- Sanguine Depths
     [709] = 381, -- Spires of Ascension
-    [713] = 376, -- The Necrotic Wake
     [717] = 382, -- Theater of Pain
     [1016] = 391, -- Tazavesh: Streets of Wonder
     [1017] = 392, -- Tazavesh: So'leah's Gambit
+    [1176] = 399, -- Ruby Life Pools
+    [1184] = 400, -- The Nokhud Offensive
+    [1180] = 401, -- The Azure Vault
     [1160] = 402, -- Algeth'ar Academy
+    [1188] = 403, -- Uldaman: Legacy of Tyr
+    [1172] = 404, -- Neltharus
     [1164] = 405, -- Brackenhide Hollow
     [1168] = 406, -- Halls of Infusion
-    [1172] = 404, -- Neltharus
-    [1176] = 399, -- Ruby Life Pools
-    [1180] = 401, -- The Azure Vault
-    [1184] = 400, -- The Nokhud Offensive
-    [1188] = 403, -- Uldaman: Legacy of Tyr
-    [1192] = 2, -- Temple of the Jade Serpent
-    [1193] = 165, -- Shadowmoon Burial Grounds
     [1195] = 438, -- The Vortex Pinnacle
+    [1274] = 456, -- Throne of the Tides
     [1247] = 463, -- Dawn of the Infinite: Galakrond's Fall
     [1248] = 464, -- Dawn of the Infinite: Murozond's Rise
-    [1274] = 456, -- Throne of the Tides
     [1281] = 499, -- Priory of the Sacred Flame
-    [1282] = 504, -- Darkflame Cleft
     [1283] = 500, -- The Rookery
-    [1284] = 503, -- Ara-Kara, City of Echoes
-    [1285] = 505, -- The Dawnbreaker
-    [1286] = 506, -- Cinderbrew Meadery
     [1287] = 501, -- The Stonevault
     [1288] = 502, -- City of Threads
+    [1284] = 503, -- Ara-Kara, City of Echoes
+    [1282] = 504, -- Darkflame Cleft
+    [1285] = 505, -- The Dawnbreaker
+    [1286] = 506, -- Cinderbrew Meadery
     [1290] = 507, -- Grim Batol
     [1550] = 525, -- Operation: Floodgate
-    [1616] = 370, -- Operation: Mechagon - Workshop
-    [1694] = 542, -- Eco-Dome Al'dani
-    [1695] = 163, -- Bloodmaul Slag Mines
     [1702] = 541, -- The Stonecore
+    [1694] = 542, -- Eco-Dome Al'dani
 };
 local missingActivityIds = {};
 
@@ -108,7 +119,7 @@ function Module:OnTooltipShow(tooltip, resultId)
     if not mapId then
         if not missingActivityIds[activityID] then
             missingActivityIds[activityID] = true;
-            Main:Print('LFG Module: no mapId found for activityID', activityID, 'please report this on curse or github');
+            Main:Print('LFG Module: no mapId found for activityID', activityID, 'please report this on discord or github');
         end
         return;
     end
